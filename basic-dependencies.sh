27cc0776247746b8062ac37146c8c59cbd49deb9
SakuraShoyo <mizzaelsantos1@gmail.com>
2021-09-30 23:51:20 -0300

    set LFS variable in lfs.sh

---
+++ basic-dependencies.sh
@@ -0,0 +1,22 @@
+!/bin/bash
+
+sudo pacman -S bash \
+  binutils \
+  bison \
+  bzip \
+  coreutils \
+  diffutils \
+  findutils\
+  gawk \
+  gcc \
+  glibc \
+  grep \
+  gzip \
+  m4 \
+  make \
+  patch \
+  perl \
+  sed \
+  tar \
+  xz \
+  texinfo
---
+++ lfs.sh
@@ -0,0 +1,6 @@
+!/bin/bash
+
+export LFS=/mnt/lfs
+export LFS_TGT=x86_64-lfs-linux-gnu
+
+
