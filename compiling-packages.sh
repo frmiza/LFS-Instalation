e3a8b5bd83085e13bb14daaab1ea6e9f32cbb3b7
Shoyo-0kbps <mizzaelsantos1@gmail.com>
2021-11-08 23:21:18 -0300

    added DIST_ROOT verifications to compiling-packages.sh

--- compiling-packages.sh
+++ compiling-packages.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "LFS: ${LFS:?}"
+echo "DIST_ROOT: ${DIST_ROOT:?}"
 
 if ! test $(whoami) == "distbuild" ; then
 	echo "must run as distbuild"
@@ -7,12 +8,10 @@ if ! test $(whoami) == "distbuild" ; then
 fi
 
 ## CHARPTER 5
-#mkdir -pv $LFS/sources/log/charpter5
-
-<<<<<<< HEAD
-#for package in binutils gcc linux-headers glibc libstdc++ ; do
-#  bash -e build_scripts/charpter5/${package}.sh | tee $LFS/sources/log/charpter5/${package}.log
-#done
+mkdir -pv $LFS/sources/log/charpter5
+for package in binutils gcc linux-headers glibc libstdc++ ; do
+  bash -e build_scripts/charpter5/${package}.sh | tee $LFS/sources/log/charpter5/${package}.log
+done
 
 ## CHARPTER 6
 
@@ -20,16 +19,3 @@ mkdir -pv $LFS/sources/log/charpter6
 for package in m4 ncurses bash coreutils diffutils file findutils gawk grep gzip make patch sed tar xz binutils gcc ; do
   bash -e build_scripts/charpter6/${package}.sh | tee $LFS/sources/log/charpter6/${package}.log
 done
-=======
-for package in binutils gcc linux-headers glibc libstdc++ ; do
-  bash -e build_scripts/charpter5/${package}.sh | tee $LFS/sources/log/charpter5/${package}.log
-done
-
-## CHARPTER 6
-
-#mkdir -pv $LFS/sources/log/charpter6
-#for package in m4 ncurses bash coreutils diffutils file findutils gawk grep gzip make patch sed tar xz binutils gcc ; do
-#  bash -e build_scripts/charpter6/${package}.sh | tee $LFS/sources/log/charpter6/${package}.log
-#done
->>>>>>> a181d103409fe8556bb43d2b0efe53a8a29f6cea
-
